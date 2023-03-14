95df1e1c433e4ed25eafcc7b6b01103883c1f18e
potatogun5000 <joguhyik6@gmail.com>
2023-03-15 04:54:13 +0700

    back rpc

--- front-end/src/contexts/ContextProvider.tsx
+++ front-end/src/contexts/ContextProvider.tsx
@@ -28,7 +28,7 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
     //const network = networkConfiguration as WalletAdapterNetwork;
     //const endpoint = useMemo(() => clusterApiUrl(network), [network]);
     const network = WalletAdapterNetwork.Mainnet;
-    const endpoint = 'https://api.mainnet-beta.solana.com';//https://rpc.helius.xyz/?api-key=7d3ccdb4-fc3d-46bb-b561-366642d291b6';
+    const endpoint = 'https://rpc.helius.xyz/?api-key=7d3ccdb4-fc3d-46bb-b561-366642d291b6';
 
     const config: ConnectionConfig = {
       commitment: 'confirmed',
