899e4d28679888a86f23e9e5d1156b635e111163
potatogun5000 <joguhyik6@gmail.com>
2023-03-16 20:04:55 +0700

    logo lol

--- front-end/src/components/AppBar.tsx
+++ front-end/src/components/AppBar.tsx
@@ -27,15 +27,9 @@ export const AppBar: React.FC = () => {
         <div className="navbar-start align-items-center">
           <div className="hidden sm:inline w-22 h-22 md:p-2 ml-10 flex-row flex">
             <div className="flex flex-row">
-              <Image
-                alt="idc"
-                src="https://media.discordapp.net/attachments/948164874561781761/1083634852382916658/x1kEpyABAgQIDAQcqBEEQgQIECAgEO3AQIECBAgMBBw6IMSRSBAgAABAg7dBggQIECAwEDAoQ9KFIEAAQIECDh0GyBAgAABAgMBhz4oUQQCBAgQIODQbYAAAQIECAwEHPqgRBEIECBAgIBDtwECBAgQIDAQcOiDEkUgQIAAAQIO3QYIECBAgMBAIE4dTvSxp7NAAAAAElFTkSuQmCC.png"
-                width={40}
-                height={40}
-              />
-              <Text variant="sub-heading" className="ml-1 pt-1">
-                solprompt
-              </Text>
+              <h1 className="shadow ml-1 pt-1">
+                șṔio
+              </h1>
             </div>
           </div>
           <WalletMultiButtonDynamic className="btn-ghost btn-sm relative flex md:hidden text-lg " />
